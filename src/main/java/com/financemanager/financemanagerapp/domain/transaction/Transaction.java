af2057cfb3c67e0d4d03be8db74d8a7e59f8c471
Fellipe Callegas <fellipe.callegas@withclutch.com>
2023-12-03 19:13:43 -0300

    Omitting installments terms on find resource

--- src/main/java/com/financemanager/financemanagerapp/domain/transaction/Transaction.java
+++ src/main/java/com/financemanager/financemanagerapp/domain/transaction/Transaction.java
@@ -1,5 +1,7 @@
 package com.financemanager.financemanagerapp.domain.transaction;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Optional;
@@ -10,6 +12,7 @@ public record Transaction(
         TransactionCategoryEnum category,
         TransactionPaymentTypeEnum paymentType,
         LocalDate date,
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
         Optional<Integer> installmentsTerms
 ) {}
 
