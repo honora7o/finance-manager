5d21952fd8da1880cba5af835931e39d7fe807e2
Fellipe Callegas <fellipe.callegas@withclutch.com>
2023-12-03 17:17:40 -0300

    Add project with a single Health endpoint resource

---
+++ src/main/java/com/financemanager/financemanagerapp/route/HealthResource.java
@@ -0,0 +1,15 @@
+package com.financemanager.financemanagerapp.route;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("health")
+public class HealthResource {
+
+    @GetMapping
+    String health() {
+        return "Application is running";
+    }
+}
---
+++ src/test/java/com/financemanager/financemanagerapp/FinancemanagerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financemanager.financemanagerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancemanagerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
