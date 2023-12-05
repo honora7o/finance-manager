182cced477d707bc52f686ed2c0ee205b91aa293
Mauricio Honorato <honorato_mauricio@hotmail.com>
2023-12-05 16:29:47 -0300

    Removed all logic and business handling from transaction repo and moved them to the respective Query class

--- src/main/java/com/financemanager/financemanagerapp/transaction/infra/TransactionRepository.java
+++ src/main/java/com/financemanager/financemanagerapp/transaction/infra/TransactionRepository.java
@@ -1,14 +1,10 @@
 package com.financemanager.financemanagerapp.transaction.infra;
 
 import com.financemanager.financemanagerapp.transaction.domain.Transaction;
-import com.financemanager.financemanagerapp.transaction.domain.TransactionCategoryEnum;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Repository
 public class TransactionRepository {
@@ -24,23 +20,5 @@ public class TransactionRepository {
 
     public List<Transaction> findAll() {
         return this.transactions;
-    }
-
-    public Map<String, BigDecimal> findMonthlyExpensesTotalByCategory(TransactionCategoryEnum category, Integer month, Integer year) {
-        Map<String, BigDecimal> result = transactions.stream()
-                .filter(transaction ->
-                        transaction.category() == category &&
-                                transaction.date().getMonthValue() == month &&
-                                transaction.date().getYear() == year)
-                .collect(Collectors.groupingBy(transaction ->
-                                String.format("%s total expenses for the period(YYYY/MM) %d/%02d",
-                                        transaction.category().toString(), year, month),
-                                Collectors.reducing(BigDecimal.ZERO, Transaction::value, BigDecimal::add)
-                ));
-
-        return result.isEmpty() ?
-                Map.of(String.format("%s total expenses for the period(YYYY/MM) %d/%02d",
-                        category.toString(), year, month), BigDecimal.ZERO) :
-                result;
     }
 }
