b9094f666c1b580e8b05da0a6c4bd762d79aa211
Mauricio Honorato <honorato_mauricio@hotmail.com>
2023-12-05 16:31:01 -0300

    Abstracted date resolution, transaction filtering, and grouping/summing logic into separate functions

--- src/main/java/com/financemanager/financemanagerapp/transaction/application/queries/findMonthlyExpensesTotalByCategoryQuery.java
+++ src/main/java/com/financemanager/financemanagerapp/transaction/application/queries/findMonthlyExpensesTotalByCategoryQuery.java
@@ -1,13 +1,16 @@
 package com.financemanager.financemanagerapp.transaction.application.queries;
 
+import com.financemanager.financemanagerapp.transaction.domain.Transaction;
 import com.financemanager.financemanagerapp.transaction.domain.TransactionCategoryEnum;
 import com.financemanager.financemanagerapp.transaction.infra.TransactionRepository;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class findMonthlyExpensesTotalByCategoryQuery {
@@ -18,9 +21,31 @@ public class findMonthlyExpensesTotalByCategoryQuery {
     }
 
     public Map<String, BigDecimal> execute(TransactionCategoryEnum category, Optional<Integer> month, Optional<Integer> year) {
-        Integer resolvedMonth = month.orElse(LocalDate.now().getMonthValue());
-        Integer resolvedYear = year.orElse(LocalDate.now().getYear());
-        return this.transactionRepository.findMonthlyExpensesTotalByCategory(category, resolvedMonth, resolvedYear);
+        LocalDate periodDate = resolveDate(month, year);
+        return groupAndSumTransactions(transactionRepository.findAll(), periodDate.getMonthValue(), periodDate.getYear(), category);
     }
 
+    private LocalDate resolveDate(Optional<Integer> month, Optional<Integer> year) {
+        return LocalDate.of(
+                year.orElse(LocalDate.now().getYear()),
+                month.orElse(LocalDate.now().getMonthValue()),
+                1
+        );
+    }
+
+    private boolean isTransactionInPeriod(Transaction transaction, int month, int year, TransactionCategoryEnum category) {
+        return transaction.category() == category &&
+                transaction.date().getMonthValue() == month &&
+                transaction.date().getYear() == year;
+    }
+
+    private Map<String, BigDecimal> groupAndSumTransactions(List<Transaction> transactions, int month, int year, TransactionCategoryEnum category) {
+        return transactions.stream()
+                .filter(transaction -> isTransactionInPeriod(transaction, month, year, category))
+                .collect(Collectors.groupingBy(
+                        transaction -> String.format("%s total expenses for the period(YYYY/MM) %d/%02d",
+                                transaction.category().toString(), year, month),
+                        Collectors.reducing(BigDecimal.ZERO, Transaction::value, BigDecimal::add)
+                ));
+    }
 }
