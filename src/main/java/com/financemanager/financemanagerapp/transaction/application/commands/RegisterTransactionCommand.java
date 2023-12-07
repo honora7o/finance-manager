7ca08fb5bb2499173736edfddb554a5de9bb4ba6
Mauricio H <honorato_mauricio@hotmail.com>
2023-12-07 00:50:09 -0300

    Added correct logging for monthly installments (#3)
    * Added correct logging for monthly installments
    
    * Implemented builder pattern to improve flexibility when creating objs of type Transaction
    
    * Refactor RegisterTransactionCommand and introduce stream for installment creation
    
    * Refactor RegisterTransactionCommand to abstract the process of building installments out of a Transaction
    
    * Remove unneded else

--- src/main/java/com/financemanager/financemanagerapp/transaction/application/commands/RegisterTransactionCommand.java
+++ src/main/java/com/financemanager/financemanagerapp/transaction/application/commands/RegisterTransactionCommand.java
@@ -4,6 +4,15 @@ import com.financemanager.financemanagerapp.transaction.domain.Transaction;
 import com.financemanager.financemanagerapp.transaction.infra.TransactionRepository;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Component
 public class RegisterTransactionCommand {
     private final TransactionRepository transactionRepository;
@@ -13,6 +22,52 @@ public class RegisterTransactionCommand {
     }
 
     public void execute(Transaction transaction) {
-        this.transactionRepository.save(transaction);
+        // maluquice??????
+        //(transaction.installmentsTerms().isPresent()
+        //        ? this.transactionRepository.saveAll(splitTransactionIntoInstallments(transaction))
+        //        : this.transactionRepository.save(transaction)
+        //);
+
+        if (transaction.installmentsTerms().isEmpty()) {
+            this.transactionRepository.save(transaction);
+            return;
+        }
+
+        List<Transaction> installments = this.splitTransactionIntoInstallments(transaction);
+        this.transactionRepository.saveAll(installments);
+    }
+
+    private List<Transaction> splitTransactionIntoInstallments(Transaction transaction) {
+        Integer installmentTermsAmount = getInstallmentTermsAmount(transaction);
+        BigDecimal installmentValue = getInstallmentValue(transaction);
+
+        return IntStream.range(0, installmentTermsAmount)
+                .mapToObj(i -> buildInstallment(transaction, i, installmentValue))
+                .collect(Collectors.toList());
+    }
+
+    private Transaction buildInstallment(Transaction transaction, int index, BigDecimal installmentValue) {
+        LocalDate installmentDate = transaction.date().plusMonths(index);
+
+        return new Transaction.TransactionBuilder()
+                .withValue(installmentValue)
+                .withDescription(buildInstallmentDescription(transaction, index))
+                .withCategory(transaction.category())
+                .withPaymentType(transaction.paymentType())
+                .withDate(installmentDate)
+                .build();
+    }
+
+    private String buildInstallmentDescription(Transaction transaction, int index) {
+        return String.format("%s %d/%d", transaction.description(), (index + 1), getInstallmentTermsAmount(transaction));
+    }
+
+    private Integer getInstallmentTermsAmount(Transaction transaction) {
+        return transaction.installmentsTerms().get();
+    }
+
+    private BigDecimal getInstallmentValue(Transaction transaction) {
+        Integer installmentTermsAmount = getInstallmentTermsAmount(transaction);
+        return transaction.value().divide(BigDecimal.valueOf(installmentTermsAmount), 2, RoundingMode.UNNECESSARY);
     }
 }
--- src/main/java/com/financemanager/financemanagerapp/transaction/domain/Transaction.java
+++ src/main/java/com/financemanager/financemanagerapp/transaction/domain/Transaction.java
@@ -14,5 +14,48 @@ public record Transaction(
         LocalDate date,
         @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
         Optional<Integer> installmentsTerms
-) {}
+) {
+    public static class TransactionBuilder {
+        private BigDecimal value;
+        private String description;
+        private TransactionCategoryEnum category;
+        private TransactionPaymentTypeEnum paymentType;
+        private LocalDate date;
+        private Optional<Integer> installmentsTerms;
+
+        public TransactionBuilder withValue(BigDecimal value) {
+            this.value = value;
+            return this;
+        }
+
+        public TransactionBuilder withDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public TransactionBuilder withCategory(TransactionCategoryEnum category) {
+            this.category = category;
+            return this;
+        }
+
+        public TransactionBuilder withPaymentType(TransactionPaymentTypeEnum paymentType) {
+            this.paymentType = paymentType;
+            return this;
+        }
+
+        public TransactionBuilder withDate(LocalDate date) {
+            this.date = date;
+            return this;
+        }
+
+        public TransactionBuilder withInstallmentsTerms(Optional<Integer> installmentsTerms) {
+            this.installmentsTerms = installmentsTerms;
+            return this;
+        }
+
+        public Transaction build() {
+            return new Transaction(value, description, category, paymentType, date, installmentsTerms);
+        }
+    }
+}
 
